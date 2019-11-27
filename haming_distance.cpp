027230044b772fbfbe9458299559be44dc56b77b
Abdul Samad <53661372+abdulsamad-optimisticstr@users.noreply.github.com>
2019-11-28 01:15:28 +0500

    uploading haming distance

---
+++ haming_distance.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int haming_distance(string str1,string str2){
+	int i=0,count=0;
+	while(i < str1.length() || i < str2.length()){
+		if(str1[i] != str2[i]){
+			count++;
+		}
+		i++;
+	}
+	return count;
+}
+int main(){
+	string str1= "Abdul";
+	string str2= "Samad";
+	int hd=haming_distance(str1,str2);
+	cout<<hd;
+	return 0;
+}
